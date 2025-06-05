9d3fd0428d18cafc19d8fccb6484d590b6bd1f99
WinterMishka <130703889+WinterMishka@users.noreply.github.com>
2025-06-05 21:02:32 +0300

    Remove automatic server launch

--- Diploma/Program.cs
+++ Diploma/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Diagnostics;
-using System.IO;
 using System.Windows.Forms;
 
 namespace Diploma
@@ -17,56 +15,27 @@ namespace Diploma
         [STAThread]
         static void Main()
         {
-            var proc = StartServer();
-            if (!PingServer())
+            if (!WaitForServer())
             {
                 MessageBox.Show("Сервер не доступен", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                try { proc?.Kill(); } catch { }
                 return;
             }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.ApplicationExit += (s, e) => { try { proc?.Kill(); } catch { } };
             Application.Run(new FaceControl());
-            try { proc?.Kill(); } catch { }
         }
 
-        private static bool PingServer()
-        {
-            try
-            {
-                using (var client = new HttpClient { Timeout = TimeSpan.FromSeconds(5) })
-                {
-                    var resp = client.GetAsync("http://127.0.0.1:5000/ping").Result;
-                    return resp.IsSuccessStatusCode;
-                }
-            }
-            catch
-            {
-                return false;
-            }
-        }
 
-        private static Process StartServer()
+        private static bool WaitForServer(int attempts = 30, int delayMs = 1000)
         {
-            try
+            for (int i = 0; i < attempts; i++)
             {
-                var scriptPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "Server", "server.py"));
-                var psi = new ProcessStartInfo
-                {
-                    FileName = "python",
-                    Arguments = $"\"{scriptPath}\"",
-                    WorkingDirectory = Path.GetDirectoryName(scriptPath),
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                };
-                return Process.Start(psi);
+                if (PingServer())
+                    return true;
+                Task.Delay(delayMs).Wait();
             }
-            catch
-            {
-                return null;
-            }
+            return false;
         }
 
         private static bool PingServer()
