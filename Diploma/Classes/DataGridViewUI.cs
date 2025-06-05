a42d401a5403830b3993d0b77f52bbaacb106a3d
WinterMishka <130703889+WinterMishka@users.noreply.github.com>
2025-06-05 20:28:09 +0300

    Add server validation and startup ping

--- Diploma/Classes/DataGridViewUI.cs
+++ Diploma/Classes/DataGridViewUI.cs
@@ -10,25 +10,30 @@ namespace Diploma.Classes
 {
     public class DataGridViewUI
     {
-        public static void BeautifyGrid(DataGridView _dataGridView)
+        public static void BeautifyGrid(DataGridView gv)
         {
-            var gv = _dataGridView;
             gv.BorderStyle = BorderStyle.None;
             gv.EnableHeadersVisualStyles = false;
             gv.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(0x2F, 0x8F, 0x8F);
             gv.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
             gv.ColumnHeadersDefaultCellStyle.Font = new Font("Verdana", 11, FontStyle.Bold);
             gv.DefaultCellStyle.Font = new Font("Verdana", 10, FontStyle.Regular);
-            gv.Columns["id_учащегося"].AutoSizeMode =
-                DataGridViewAutoSizeColumnMode.AllCells;
-            gv.Columns["ФИО"].AutoSizeMode =
-                DataGridViewAutoSizeColumnMode.Fill;
-            gv.Columns["Даты_отсутствия"].AutoSizeMode =
-                DataGridViewAutoSizeColumnMode.Fill;
-            gv.Columns["Даты_отсутствия"].DefaultCellStyle.WrapMode =
-                DataGridViewTriState.True;
-            gv.Columns["Сумма_пропусков"].AutoSizeMode =
-                DataGridViewAutoSizeColumnMode.AllCells;
+
+            if (gv.Columns.Contains("id_учащегося"))
+                gv.Columns["id_учащегося"].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+
+            if (gv.Columns.Contains("ФИО"))
+                gv.Columns["ФИО"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            if (gv.Columns.Contains("Даты_отсутствия"))
+            {
+                gv.Columns["Даты_отсутствия"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                gv.Columns["Даты_отсутствия"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            }
+
+            if (gv.Columns.Contains("Сумма_пропусков"))
+                gv.Columns["Сумма_пропусков"].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+
             gv.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
             gv.RowHeadersVisible = false;
             gv.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(0xE8, 0xF5, 0xE9);
