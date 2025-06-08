e541a50ccc29d48c4a9dc1c4d1793835f9c22f64
WinterMishka <130703889+WinterMishka@users.noreply.github.com>
2025-06-08 20:01:06 +0300

    Clear group curator links after deleting groups

--- Diploma/Classes/DbDeleteService.cs
+++ Diploma/Classes/DbDeleteService.cs
@@ -106,6 +106,8 @@ namespace Diploma.Helpers
                 DeleteFolderIfExists(folderPath);
             }
 
+
+            DeleteGroupsForCurator(id);
             ClearGroupCurators(id);
 
             using (var con = new SqlConnection(_mgr.ConnStr))
@@ -270,6 +272,33 @@ namespace Diploma.Helpers
             using (var cmd = new SqlCommand("DELETE FROM Группа WHERE id_код = @code", con))
             {
                 cmd.Parameters.AddWithValue("@code", codeId);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void DeleteGroupsForCurator(int curatorId)
+        {
+            using (var con = new SqlConnection(_mgr.ConnStr))
+            using (var cmd = new SqlCommand("SELECT id_группы FROM Группа WHERE id_сотрудника = @cur", con))
+            {
+                cmd.Parameters.AddWithValue("@cur", curatorId);
+                con.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    var ids = new List<int>();
+                    while (reader.Read())
+                        ids.Add(reader.GetInt32(0));
+
+                    foreach (int gid in ids)
+                        DeleteStudentsWhere("id_группы", gid);
+                }
+            }
+
+            using (var con = new SqlConnection(_mgr.ConnStr))
+            using (var cmd = new SqlCommand("DELETE FROM Группа WHERE id_сотрудника = @cur", con))
+            {
+                cmd.Parameters.AddWithValue("@cur", curatorId);
                 con.Open();
                 cmd.ExecuteNonQuery();
             }
